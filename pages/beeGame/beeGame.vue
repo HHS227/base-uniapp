c3efb6085c6c79da6132b622a74fba04c50266c1
HHS <1339084443@qq.com>
2025-05-16 18:01:47 +0800

    feat(蜜蜂游戏): 添加游戏背景和右上角按钮
    在蜜蜂游戏页面中添加了游戏背景图片和右上角按钮的布局及样式，以提升游戏界面的视觉效果和用户体验。

--- pages/beeGame/beeGame.vue
+++ pages/beeGame/beeGame.vue
@@ -1,6 +1,11 @@
 <template>
-    <view>
-		养蜂游戏
+    <view class="container">
+		<image src="/static/images/游戏背景.png" mode="" class="game-bg"></image>
+		<view class="right-top-btn">
+			<image src="/static/images/群组 2 jQlrI3N@1x.png" mode=""></image>
+		</view>
+		<view class="broadcast-btn">
+		</view>
     </view>
 </template>
 
@@ -8,4 +13,25 @@
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	.container{
+		position: relative;
+		z-index: 1;
+		height: 100vh;
+		.game-bg{
+			position: absolute;
+			z-index: -1;
+			width: 100%;
+			height: 100%;
+		}
+		.right-top-btn{
+			position: absolute;
+			right:  30rpx;
+			top: 220rpx;
+			image{
+				width: 112rpx;
+				height: 110rpx;
+			}
+		}
+	}
+</style>
