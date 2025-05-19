ab236427b3ea9bd75e3f8b35275c68ee01bc32b5
HHS <1339084443@qq.com>
2025-05-19 14:58:16 +0800

    feat(通知组件): 添加Notice组件并集成到首页和蜜蜂游戏页面
    添加了新的Notice组件，用于显示滚动通知，并将其集成到首页和蜜蜂游戏页面中。同时更新了相关页面的图片资源，删除了不再使用的旧图片。

--- pages/beeGame/beeGame.vue
+++ pages/beeGame/beeGame.vue
@@ -1,37 +1,73 @@
 <template>
-    <view class="container">
+	<view class="container">
 		<image src="/static/images/游戏背景.png" mode="" class="game-bg"></image>
 		<view class="right-top-btn">
-			<image src="/static/images/群组 2 jQlrI3N@1x.png" mode=""></image>
+			<image src="/static/images/攻略.png" mode=""></image>
+			<image src="/static/images/蜂场活动.png" mode=""></image>
+			<image src="/static/images/兑换M币.png" mode=""></image>
 		</view>
 		<view class="broadcast-btn">
+			<image src="/static/images/广播.png" mode=""></image>
+			<text>收获广播</text>
+			
 		</view>
-    </view>
+		<view class="broadcast-content">
+			<NoticeVue :notices="['通知1', '通知2', '通知3', '通知4']" />
+		</view>
+	</view>
 </template>
 
 <script setup>
-
+import NoticeVue from '@/components/Notice.vue';
 </script>
 
 <style lang="scss" scoped>
-	.container{
-		position: relative;
-		z-index: 1;
-		height: 100vh;
-		.game-bg{
-			position: absolute;
-			z-index: -1;
-			width: 100%;
-			height: 100%;
+.container {
+	position: relative;
+	z-index: 1;
+	height: 100vh;
+	.game-bg {
+		position: absolute;
+		z-index: -1;
+		width: 100%;
+		height: 100%;
+	}
+	.broadcast-btn {
+		position: absolute;
+		left: 30rpx;
+		top: 196rpx;
+		width: 168rpx;
+		height: 50rpx;
+		border-radius: 30rpx;
+		background: #fff;
+		display: flex;
+		align-items: center;
+		image {
+			width: 50rpx;
+			height: 50rpx;
 		}
-		.right-top-btn{
-			position: absolute;
-			right:  30rpx;
-			top: 220rpx;
-			image{
-				width: 112rpx;
-				height: 110rpx;
-			}
+		text {
+			
+			color: rgba(155, 113, 32, 1);
+			font-size: 24rpx;
 		}
 	}
+	.broadcast-content{
+		position: absolute;
+		left: 30rpx;
+		top: 270rpx;
+		width: 356rpx;
+		height: 188rpx;
+	}
+	.right-top-btn {
+		position: absolute;
+		right: 30rpx;
+		top: 220rpx;
+		image {
+			margin-top: 24rpx;
+			width: 112rpx;
+			height: 110rpx;
+		}
+	}
+}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,13 +4,16 @@
 		<view class="text-area">
 			<text class="title">{{ title }}</text>
 		</view>
+		<NoticeVue :notices="['通知1', '通知2', '通知3']" />
 		<button open-type="getPhoneNumber" @getphonenumber="getPhone" :ref="btnRef" class="login-btn">点击登录</button>
 		<avatarStack :avatars="avatarList" :size="40" :overlap="20"></avatarStack>
 	</view>
 </template>
 
 <script lang="ts" setup>
+
 import { ref } from 'vue';
+import NoticeVue from '../../components/Notice.vue';
 import avatarStack from '../../components/AvatarStack.vue'
 const title = ref('测试');
 const wxLoginData = ref({
